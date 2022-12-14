01504c7205c48a7b3cdafddfb2ae1bcd98fb5a73
Saulo_Rios <40133707+saulorios@users.noreply.github.com>
2022-12-14 18:04:50 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,11 +39,18 @@ public class Program {
 			System.out.println(obj);
 		}
 				
+//		System.out.println();
+//		System.out.println("=== Seller Insert ===");
+//		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+//		sellerDao.insert(newSeller);
+//		System.out.println("Inserted! New id: " + newSeller.getId());
+		
 		System.out.println();
-		System.out.println("=== Seller Insert ===");
-		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
-		sellerDao.insert(newSeller);
-		System.out.println("Inserted! New id: " + newSeller.getId());
+		System.out.println("=== Seller Update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Bruce Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 
 }
