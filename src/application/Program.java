933d0c6e2036f841af533f29ee6dd91b0d1f440e
Saulo_Rios <40133707+saulorios@users.noreply.github.com>
2022-12-14 17:47:16 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+
+import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
@@ -37,6 +39,11 @@ public class Program {
 			System.out.println(obj);
 		}
 				
+		System.out.println();
+		System.out.println("=== Seller Insert ===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id: " + newSeller.getId());
 	}
 
 }
